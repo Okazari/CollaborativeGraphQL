a07551387dc1015217855c49551b7affbd1d399f
Plouzennec Benjamin <plouzennec.benjamin@gmail.com>
2018-03-28 23:01:42 +0200

    :nail_care: some styles

--- notifications-app/client/src/components/Notification/NotificationComponent.js
+++ notifications-app/client/src/components/Notification/NotificationComponent.js
@@ -1,6 +1,60 @@
 import React from 'react';
 
-export default ({ data, loading }) => 
-<div>
-    { loading ? 'loading...' : <div>{data.messageAdded.content}</div> }
-</div> 
+const messageStyle = {
+  backgroundColor: 'lavender',
+  padding: 5,
+  margin: 5,
+  width: 300,
+}
+
+const userStyle = {
+  color: 'grey',
+  margin: 5,
+  padding: 5,
+  borderBottom: '1px solid',
+}
+
+const contentStyle = {
+  margin: 5,
+  padding: 5,
+  paddingLeft: 15,
+  fontSize: 24,
+}
+
+const containerStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  width: 400,
+  margin: 'auto',
+}
+
+const buildMessage = (m, i) => {
+  let s = { ...messageStyle }
+  s.transform = `translateX(${i%2?50:-50}px)`
+  if(i%2 == 0) s.backgroundColor = 'aliceblue'
+  return (
+    <div style={s}>
+      <div style={{ ...userStyle }}>
+        {m.user}
+      </div>
+      <div style={{ ...contentStyle }}>
+        {m.content}
+      </div>
+    </div>
+  )
+}
+
+export default ({ data, loading }) => {
+ const messages = []
+ return (
+   <div>
+     <div style={{ ... containerStyle}}>
+         {
+           !loading ? messages.map(buildMessage)
+           : 'Aucun message'
+         }
+     </div>
+   </div>
+ )
+}
